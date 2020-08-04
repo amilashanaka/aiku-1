bf88d5f2026989795c44c51d312c87614373cafc
Raul Perusquia <raul@inikoo.com>
2020-08-04 20:42:52 +0800

    Address fiels scalfolding

--- database/migrations/2020_08_04_121316_create_addresses_table.php
+++ database/migrations/2020_08_04_121316_create_addresses_table.php
@@ -4,19 +4,37 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAddressesTable extends Migration
-{
+class CreateAddressesTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
-        Schema::create('addresses', function (Blueprint $table) {
+    public function up() {
+
+        //https://github.com/commerceguys/addressing
+        /*
+            Country code (The two-letter country code)
+            Administrative area
+            Locality (City)
+            Dependent Locality
+            Postal code
+            Sorting code
+            Address line 1
+            Address line 2
+            Organization
+            Given name (First name)
+            Additional name (Middle name / Patronymic)
+            Family name (Last name)
+
+         */
+
+        Schema::create(
+            'addresses', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-        });
+        }
+        );
     }
 
     /**
@@ -24,8 +42,7 @@ class CreateAddressesTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('addresses');
     }
 }
